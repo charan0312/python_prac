f402214017668f74611bda2b243230e4573a33fe
Charan Parvathareddy <charan03.12@gmail.com>
2019-04-27 05:37:36 -0500

    Modified classes.py

--- classes.py
+++ classes.py
@@ -179,6 +179,300 @@ c.circumference
 
 # Calling a Method on a Parent Class
 # To call a method in a parent (or superclass), use the super() function
+class A:
+    def __init__(self):
+        self.x = 0
+class B(A):
+    def __init__(self):
+        super().__init__()  # super makes sure that parents are initialized properly
+        self.y = 1
+        
+b = B()
+b.x
+
+# Multiple inheritance and super()
+class Base:
+    def __init__(self):
+        print('Hello from Base')
+
+class A(Base):
+    def __init__(self):
+        super().__init__()
+        print('Hello from A')
+        
+class B(Base):
+    def __init__(self):
+        super().__init__()
+        print('Hello from B')
+        
+class C(A,B):
+    def __init__(self):
+        super().__init__()    # only one cal from super 
+        print('Hello from C')
+    
+c = C()
+c
+
+# method resolution order (MRO) is simply a linear ordering of all the base classes (C3 Linearization)
+# To implement inheritance, Python starts with the leftmost class and works its way leftto-
+# right through classes on the MRO list until it finds the first attribute match
+C.__mro__
+
+# C3 Linearization is a merge sort of MROs from parent classes with 3 constraints:
+# Child classes are checked before parent classes
+# Multiple parents are checked in the order listed
+# If there are 2 valid choices for the next class, pick one from first parent
+class A:
+    def spam(self):
+        print('Hello from A')
+        super().spam()
+                
+class B:
+    def spam(self):
+        print('Hello from B')
+        
+class C(A,B):
+    pass
+
+C.__mro__
+c = C()
+c.spam()
+
+
+
+# Extending a Property in a Subclass
+class Person:
+    def __init__(self, name):
+        self.name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, val):
+        if not isinstance(val, str):
+            raise TypeError('Expected a string')
+        self._name = val
+    
+    @name.deleter
+    def name(self):
+        raise AttributeError('Can\'t delete attribute')
+        
+class SubPerson(Person):
+    
+    @property
+    def name(self):
+        print('getting name')
+        return super(SubPerson, SubPerson).name.__get__(self)
+    
+    @name.setter
+    def name(self, value):
+        print('setting name to', value)
+        super(SubPerson, SubPerson).name.__set__(self,value)
+
+    @name.deleter
+    def name(self):
+        print('Deleting name')
+        super(SubPerson, SubPerson).name.__delete__(self)
+
+s = SubPerson('abcd')
+s.name
+
+# To extend just one of the methods of a property use @ClassA.methodA.getter etc
+class SubPerson(Person):
+    @Person.name.getter # set the decorator like this
+    def name(self):
+        print('Getting name')
+        return super().name
+    
+    
+    
+# Using Lazily Computed Properties
+# To define a read-only attribute as a property that only gets computed on access.
+# Once accessed, you’d like the value to be cached and not recomputed on each access.
+class lazyproperty: # This is a descriptor class as in the form of __get__(), __set__(), and __delete__() special methods. 
+    def __init__(self, func):
+        self.func = func
+    
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            value = self.func(instance)
+            setattr(instance, self.func.__name__, value)
+            return value            
+
+# 
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    @lazyproperty
+    def area(self):
+        print('Computing area')
+        return math.pi * self.radius ** 2
+    @lazyproperty
+    def perimeter(self):
+        print('Computing perimeter')
+        return 2 * math.pi * self.radius    
+
+c = Circle(5)
+vars(c)
+c.area
+c.perimeter
+vars(c)
+c.area # Not computed 2nd time
+
+
+
+# Simplifying the Initialization of Data Structures
+# To avoid writing highly repetitive and boilerplate __init__() functions
+# generalize the initialization of data structures into a single __init__()
+# function defined in a common base class
+
+class Structure:
+    # Class variable that specifies expected fields
+    _fields = []
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+            
+        
+#  Example Class definitions
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+        
+    class Point(Structure):
+        _fields = ['x', 'y']
+        
+    class Circle(Structure):
+        _fields = ['radius']
+        def area(self):
+            return math.pi * self.radius ** 2
+
+s = Stock('ACME', 50, 91.1)
+p = Point(2, 3)
+c = Circle(4.5)
+s2 = Stock('ACME', 50)
+
+# keyword arguments as a means for adding additional
+# attributes to the structure not specified in _fields.
+class Structure:
+# Class variable that specifies expected fields
+    _fields= []
+    def __init__(self, *args, **kwargs):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)      # self.__dict__.update(zip(self._fields,args))
+        # Set the additional arguments (if any)
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+        if kwargs:
+            raise TypeError('Duplicate values for {}'.format(','.join(kwargs)))
+# Example use
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+
+    s1 = Stock('ACME', 50, 91.1)
+    s2 = Stock('ACME', 50, 91.1, date='8/2/2012')
+
+
+# Implementing a Data Model or Type System
+# placing checks or assertions on the setting of certain instance attributes
+# Base class. Uses a descriptor to set a value
+class Descriptor:
+    def __init__(self, name=None, **opts):
+        self.name = name
+        for key, value in opts.items():
+            setattr(self, key, value)
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+# Descriptor for enforcing types
+class Typed(Descriptor):
+    expected_type = type(None)
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('expected ' + str(self.expected_type))
+        super().__set__(instance, value)
+# Descriptor for enforcing values
+class Unsigned(Descriptor):
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        super().__set__(instance, value)
+class MaxSized(Descriptor):
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super().__init__(name, **opts)
+    def __set__(self, instance, value):
+        if len(value) >= self.size:
+            raise ValueError('size must be < ' + str(self.size))
+        super().__set__(instance, value)
+
+# These classes should be viewed as basic building blocks from which you construct a data
+# model or type system. Continuing, here is some code that implements some different kinds of data
+class Integer(Typed):
+    expected_type = int
+class UnsignedInteger(Integer, Unsigned):
+    pass
+class Float(Typed):
+    expected_type = float
+class UnsignedFloat(Float, Unsigned):
+    pass
+class String(Typed):
+    expected_type = str
+class SizedString(String, MaxSized):
+    pass
+
+# Using these type objects, it is now possible to define a class such as this
+class Stock:
+    # Specify constraints
+    name = SizedString('name',size=8)
+    shares = UnsignedInteger('shares')
+    price = UnsignedFloat('price')
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+s = Stock('ACME', 50, 91.1)
+s.name
+s.shares = -10
+s.name = 'ABRACADABRA'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
