b829a4ed4789a59e047f529470ffb5dd573aa1e6
Charan Parvathareddy <charan03.12@gmail.com>
2025-03-03 23:39:35 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,128 +1,88 @@
+SELECT 
+    -- Practitioner Basic Information
+    p.practitionerid,
+    p.practitionertypeid,
+    pt.practitionertypename,
+    p.firstname,
+    p.middlename,
+    p.lastname,
+    p.suffixid,
+    p.birthdate,
+    p.nationalproviderid,
+    p.statustypeid,
+    st.statustypename,
+    ss.statussetname,
+    g.gendername,
 
+    -- Practitioner Languages
+    STRING_AGG(l.languagename, ', ') AS languages,
 
-REPLACE PROCEDURE  HSLABCORNERSTONE.SP_DMOM_sDIRECTORY_OSS_DENTAL_PROVIDER_DETAILS_RECON()
+    -- Education
+    STRING_AGG(
+        pe.institution || ' (' || d.degreename || ')', ', '
+    ) AS education_details,
 
-SQL SECURITY INVOKER
-MAIN: BEGIN
-DECLARE EXIT HANDLER
-   FOR SQLEXCEPTION
-    BEGIN
-      INSERT INTO HSLABCORNERSTONE.DMOM_SDIR_RECON_LOAD_LOGS VALUES('DENTAL_PROVIDER_DETAILS_RECON',CURRENT_TIMESTAMP,-1,'SQLSTATE : '||:SQLSTATE||' SQLCODE : '||:SQLCODE);
-    END;
+    -- Specialties
+    STRING_AGG(s.specialtyname, ', ') AS specialties,
 
+    -- Certifications
+    STRING_AGG(cb.boardname, ', ') AS certification_boards,
 
-INSERT INTO HSLABCORNERSTONE.DMOM_SDIR_RECON_LOAD_LOGS VALUES ('DENTAL_PROVIDER_DETAILS_RECON',CURRENT_TIMESTAMP,1,'DENTAL_PROVIDER_DETAILS_RECON Begin');
+    -- Taxonomies
+    STRING_AGG(t.taxonomycode, ', ') AS taxonomy_codes
 
+FROM practitioners p
+-- Join Practitioner Type
+LEFT JOIN practitionertypes pt 
+    ON p.practitionertypeid = pt.practitionertypeid
 
--- DELETE EXISITNG DATA FROM TABLE
-DELETE FROM HSLABCORNERSTONE.DMOM_SDIRECTORY_DENTAL_PROVIDER_DETAILS_FALLOUT;
+-- Join Gender
+LEFT JOIN genders g 
+    ON p.genderid = g.genderid
 
--- INSERT RECORDS FROM Source TABLE
-INSERT INTO HSLABCORNERSTONE.DMOM_SDIRECTORY_DENTAL_PROVIDER_DETAILS_FALLOUT
-SELECT ob_pd.*,
-       CASE
-          WHEN oss_pd.ProviderID IS NULL
-          THEN
-             'Record Not Matching OSS'
-          WHEN COALESCE (ob_pd."First Name", '') <>
-               COALESCE (oss_pd.HsFirstName, '')
-          THEN
-             'First Name Not matching in OSS'
-          WHEN COALESCE (ob_pd."Middle Name", '') <>
-               COALESCE (oss_pd.HsMiddleName, '')
-          THEN
-             'Middle Name Not matching in OSS'
-          WHEN COALESCE (ob_pd."Last Name", '') <>
-               COALESCE (oss_pd.HsLastName, '')
-          THEN
-             'Last Name Not matching in OSS'
-          WHEN COALESCE (ob_pd."Professional Title", '') <>
-               COALESCE (oss_pd.ProfessionalTitle, '')
-          THEN
-             'Professional Title Not matching in OSS'
-          WHEN COALESCE (ob_pd."Gender", '') <> COALESCE (oss_pd.Gender, '')
-          THEN
-             'Gender Not matching in OSS'
-          WHEN COALESCE (ob_pd."Language 1", '') <>
-               COALESCE (oss_pd.Language1, '')
-          THEN
-             'Language 1 Not matching in OSS'
-          WHEN COALESCE (ob_pd."Language 2", '') <>
-               COALESCE (oss_pd.Language2, '')
-          THEN
-             'Language 2 Not matching in OSS'
-          WHEN COALESCE (ob_pd."Language 3", '') <>
-               COALESCE (oss_pd.Language3, '')
-          THEN
-             'Language 3 Not matching in OSS'
-       END AS comments
-FROM DMOM_OB_DENTAL_PROVIDER_DETAILS ob_pd
-     LEFT JOIN
-     (SELECT pa_den.ProviderID,
-             pa_den.HealthSystemID,
-             pa_den.ProfessionalTitle,
-             pa_den.Gender,
-             pa_den.HsFirstName,
-             pa_den.HsMiddleName,
-             pa_den.HsLastName,
-             p_l.Language1,
-             p_l.Language2,
-             p_l.Language3
-      FROM (SELECT pa_1.ProviderID,
-                   pa_1.HealthSystemID,
-                   pa_1.DisplayFirstName,
-                   pa_1.DisplayMiddleName,
-                   pa_1.DisplayLastName,
-                   pa_1.ProfessionalTitle,
-                   pa_1.Gender,
-                   pa_1.HsFirstName,
-                   pa_1.HsMiddleName,
-                   pa_1.HsLastName
-            FROM HSLABCORNERSTONE.PROV_SDIR_ProviderAttribute pa_1
-                 INNER JOIN
-                 HSLABCORNERSTONE.PROV_SDIR_ProviderLocation ploc_1
-                    ON pa_1.ProviderID = ploc_1.ProviderID
-                 INNER JOIN HSLABCORNERSTONE.PROV_SDIR_Location loc_1
-                    ON ploc_1.LocationID = loc_1.LocationID
-            WHERE loc_1.locationtypeid = 721) pa_den
-           INNER JOIN
-           (SELECT p.ProviderID,
-                   MAX (CASE WHEN rn = 1 THEN p.languagename END)
-                      AS Language1,
-                   MAX (CASE WHEN rn = 2 THEN p.languagename END)
-                      AS Language2,
-                   MAX (CASE WHEN rn = 3 THEN p.languagename END)
-                      AS Language3
-            FROM (SELECT pl.ProviderID,
-                         l.LanguageName,
-                         ROW_NUMBER ()
-                         OVER (PARTITION BY pl.providerid
-                               ORDER BY l.languagename) AS rn
-                  FROM HSLABCORNERSTONE.PROV_SDIR_ProviderLanguage pl
-                       LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_Language l
-                          ON pl.LanguageID = l.LanguageID) p
-            GROUP BY p.providerid) p_l
-              ON pa_den.ProviderID = p_l.ProviderID) oss_pd
-        ON ob_pd."Provider ID" = oss_pd.ProviderID
-WHERE    oss_pd.ProviderID IS NULL
-      OR COALESCE (ob_pd."First Name", '') <>
-         COALESCE (oss_pd.HsFirstName, '')
-      OR COALESCE (ob_pd."Middle Name", '') <>
-         COALESCE (oss_pd.HsMiddleName, '')
-      OR COALESCE (ob_pd."Last Name", '') <> COALESCE (oss_pd.HsLastName, '')
-      OR COALESCE (ob_pd."Professional Title", '') <>
-         COALESCE (oss_pd.ProfessionalTitle, '')
-      OR COALESCE (ob_pd."Gender", '') <> COALESCE (oss_pd.Gender, '')
-      OR -- COALESCE(ob_pd."Primary Specialty", '') <> COALESCE(oss_pd.PrimarySpecialty, '') OR
-         -- COALESCE(ob_pd."Specialty 2", '') <> COALESCE(oss_pd.Specialty2, '') OR
-         -- COALESCE(ob_pd."Specialty 3", '') <> COALESCE(oss_pd.Specialty3, '') OR
-         -- COALESCE(ob_pd."Specialty 4", '') <> COALESCE(oss_pd.Specialty4, '') OR
-         -- COALESCE(ob_pd."Specialty 5", '') <> COALESCE(oss_pd.Specialty5, '') OR
-         COALESCE (ob_pd."Language 1", '') <> COALESCE (oss_pd.Language1, '')
-      OR COALESCE (ob_pd."Language 2", '') <> COALESCE (oss_pd.Language2, '')
-      OR COALESCE (ob_pd."Language 3", '') <> COALESCE (oss_pd.Language3, '');
+-- Join Status
+LEFT JOIN statustypes st 
+    ON p.statustypeid = st.statustypeid
+LEFT JOIN statussets ss 
+    ON st.statustypeid = ss.statussetid
 
+-- Join Languages
+LEFT JOIN practitionerlanguages pl 
+    ON p.practitionerid = pl.practitionerid
+LEFT JOIN languages l 
+    ON pl.languageid = l.languageid
 
-INSERT INTO HSLABCORNERSTONE.DMOM_SDIR_RECON_LOAD_LOGS VALUES ('DENTAL_PROVIDER_DETAILS_RECON',CURRENT_TIMESTAMP,2,'DENTAL_PROVIDER_DETAILS_RECON Completed');
+-- Join Education
+LEFT JOIN practitionereducation pe 
+    ON p.practitionerid = pe.practitionerid
+LEFT JOIN degrees d 
+    ON pe.degreeid = d.degreeid
 
-END;
+-- Join Specialties
+LEFT JOIN practitionorspecialties ps 
+    ON p.practitionerid = ps.practitionerid
+LEFT JOIN specialties s 
+    ON ps.specialtyid = s.specialtyid
+
+-- Join Certifications
+LEFT JOIN certificationboards cb 
+    ON s.certificationboardid = cb.certificationboardid
+
+-- Join Taxonomies
+LEFT JOIN taxonomies t 
+    ON s.specialtyid = t.specialtyid
+
+GROUP BY 
+    p.practitionerid, 
+    p.practitionertypeid, 
+    pt.practitionertypename,
+    p.firstname, 
+    p.middlename, 
+    p.lastname, 
+    p.suffixid, 
+    p.birthdate, 
+    p.nationalproviderid, 
+    p.statustypeid,
+    st.statustypename,
+    ss.statussetname,
+    g.gendername;
