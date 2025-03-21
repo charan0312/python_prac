3d8f8393986733512e159b53552567214a073331
Charan Parvathareddy <charan03.12@gmail.com>
2025-03-21 18:15:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-Fix this
+from pyspark.sql.functions import explode
 
-dental_info_df = dental_info_df.select(explode(dental_info_df.networkcode).alias("Plan ID").filter("Plan ID".isin(["CD005", "CP023", "PA027"])))
+dental_info_df = (
+    dental_info_df
+    .select(explode(dental_info_df.networkcode).alias("Plan ID"))
+    .filter(dental_info_df["Plan ID"].isin(["CD005", "CP023", "PA027"]))
+)
