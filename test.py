1b4d94b2cc02dac64f49908077301cd516d0136e
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-15 17:17:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,16 @@
-SELECT DISTINCT ls.*,
-       CASE
-          WHEN sdl.ExternalCode IS NULL
-          THEN
-             'Record Missing in OSS'
-          WHEN
-          COALESCE (st.ServiceTypeName, '') =
-              COALESCE (sdlft.featuretypename, '')
-          THEN
-             'Service Type Not matching in OSS'
-       END AS comments
+SELECT DISTINCT pl.*,
+  CASE
+    WHEN sdl.ExternalCode IS NULL THEN 'Record Missing in OSS'
+    
+    WHEN COALESCE(a.LineNumber1, '') <> COALESCE(sdla.Address1, '') THEN 'Address Not matching in OSS'
 
-FROM PROVIDERDATASERVICE_CORE_V.PROV_SPAYER_LOCATIONSERVICES ls
-LEFT JOIN PROVIDERDATASERVICE_CORE_V.PROV_SPAYER_SERVICETYPES st
-ON ls.ServiceTypeID = st.ServiceTypeID
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_Location sdl
-ON ls.LocationID = sdl.ExternalCode
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeature sdlf
-ON sdl.LocationID = sdlf.LocationID
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeatureItem sdlfi
-ON sdlf.FeatureId = sdlfi.LocationFeatureItemId
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeatureType sdlft
-ON sdlft.LocationFeatureTypeId = sdlft.LocationFeatureTypeId
-WHERE 
-sdl.ExternalCode IS NULL
-OR COALESCE (st.ServiceTypeName, '') =
-              COALESCE (sdlft.featuretypename, '');
-
-
-
-
-SELECT DISTINCT
-    ls.*,
-    CASE
-        WHEN sdl.ExternalCode IS NULL THEN 'Record Missing in OSS'
-        WHEN COALESCE(st.ServiceTypeName, '') <> COALESCE(sdlft.featuretypename, '') THEN 'Service Type Not matching in OSS'
-    END AS comments
-FROM PROVIDERDATASERVICE_CORE_V.PROV_SPAYER_LOCATIONSERVICES ls
-LEFT JOIN PROVIDERDATASERVICE_CORE_V.PROV_SPAYER_SERVICETYPES st
-    ON ls.ServiceTypeID = st.ServiceTypeID
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_Location sdl
-    ON TRIM(ls.LocationID) = TRIM(sdl.ExternalCode)
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeature sdlf
-    ON sdl.LocationID = sdlf.LocationID
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeatureItem sdlfi
-    ON sdlf.FeatureId = sdlfi.LocationFeatureItemId
-LEFT JOIN HSLABCORNERSTONE.PROV_SDIR_LocationFeatureType sdlft
-    ON sdlfi.LocationFeatureTypeId = sdlft.LocationFeatureTypeId
-WHERE
-    sdl.ExternalCode IS NULL
-    OR COALESCE(st.ServiceTypeName, '') <> COALESCE(sdlft.featuretypename, '');
+    WHEN COALESCE(
+           CASE 
+             WHEN TRIM(UPPER(pl.TwentyFourHourCoverage)) = 'Y' THEN '1'
+             WHEN TRIM(UPPER(pl.TwentyFourHourCoverage)) = 'N' THEN '0'
+             ELSE ''
+           END, ''
+         ) 
+         <> COALESCE(CAST(sdls.isopen24hours AS VARCHAR(1)), '') THEN '24-Hour Coverage Not matching in OSS'
+         
+  END AS comments
