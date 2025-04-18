3c9528f79f0aa0490b572c744a2b5f563f373194
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 23:21:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
-SELECT * 
-FROM (
-    SELECT 
-        CAST(sdnu.NPI AS VARCHAR(100)) AS NPI,
-        CAST(sdnu.SpecialityName AS VARCHAR(100)) AS SpecialityName,
-        CONCAT(CAST(sdnu.NPI AS VARCHAR(100)), '||', LOWER(TRIM(CAST(sdnu.SpecialityName AS VARCHAR(100))))) AS sdnu_concat
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu
-    WHERE sdnu.domainname = 'sPayer'
+fix this
+
+select a.npi, a.qnxt_specialty, a.npiu2_concat,
+case when a.qnxt_specialty is null then a.npiu2_concat
+else concat(cast(a.npi as VARCHAR(100)), '||' ,LOWER(TRIM(a.qnxt_specialty))) 
+end as test
+
+from
+(
+SELECT 
+        CAST(npiu2.NPI AS VARCHAR(100)) AS NPI,
+        CAST('' AS VARCHAR(100)) AS qnxt_specialty,
+        CONCAT(CAST(npiu2.NPI AS VARCHAR(100)), '||A') AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
 
     UNION
 
     SELECT 
-        CAST(sdnu.NPI AS VARCHAR(100)) AS NPI,
-        CAST(sdnu.SpecialityName AS VARCHAR(100)) AS SpecialityName,
-        CONCAT(CAST(sdnu.NPI AS VARCHAR(100)), '||A') AS sdnu_concat
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu
-    WHERE sdnu.domainname = 'sPayer'
-) a
-ORDER BY 1;
+        CAST(npiu1.NPI AS VARCHAR(100)) AS NPI,
+        CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
+        CAST('' AS VARCHAR(100)) AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
+    ) a
+   order by 1
