8a3a993f9453dd49b78816ba00a9bf8a96aea987
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 22:49:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,9 @@
--- Full fallout classification using WHERE clause logic
-SELECT 
-    A.NPI,
-    A.SpecialityName,
-    'NPI Missing' AS COMMENT
-FROM HSLABCORNERSTONE.Directory_NPI_Universe A
-WHERE A.NPI NOT IN (
-    SELECT DISTINCT NPI
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE
-    WHERE DomainName = 'sPayer'
-)
+below is my query I'm getting nulls in specialty columns foix it for teradata
 
-UNION ALL
-
-SELECT 
-    A.NPI,
-    A.SpecialityName,
-    'Speciality Not Matching' AS COMMENT
-FROM HSLABCORNERSTONE.Directory_NPI_Universe A
-WHERE A.NPI IN (
-    SELECT DISTINCT NPI
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE
-    WHERE DomainName = 'sPayer'
-)
-AND (A.NPI, LOWER(TRIM(A.SpecialityName))) NOT IN (
-    SELECT B.NPI, LOWER(TRIM(B.SpecialityName))
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE B
-    WHERE DomainName = 'sPayer'
-)
-
-UNION ALL
-
-SELECT 
-    A.NPI,
-    A.SpecialityName,
-    'Matched' AS COMMENT
-FROM HSLABCORNERSTONE.Directory_NPI_Universe A
-WHERE (A.NPI, LOWER(TRIM(A.SpecialityName))) IN (
-    SELECT B.NPI, LOWER(TRIM(B.SpecialityName))
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE B
-    WHERE DomainName = 'sPayer'
-);
+select * from
+(SELECT npiu2.NPI,'' as qnxt_specialty,
+concat(cast(npiu2.NPI as VARCHAR(100)), '||A' ) as npiu2_concat
+FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
+union all
+select npiu1.NPI, npiu1.qnxt_specialty , '' as npiu1_concat from HSLABCORNERSTONE.Directory_NPI_Universe npiu1) a
+order by 1
