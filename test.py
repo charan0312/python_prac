fb3857a5ced564ae417cee65e7be8a7558f7c790
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 23:34:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 fix this
 
-select a.npi, a.qnxt_specialty, a.npiu2_concat,
-case when a.qnxt_specialty is null then a.npiu2_concat
-else concat(cast(a.npi as VARCHAR(100)), '||' ,LOWER(TRIM(a.qnxt_specialty))) 
-end as test
 
+select top 5 * from 
+(
+select a.npi, 
+case when a.qnxt_specialty = '' then a.npiu2_concat
+else concat(cast(a.npi as VARCHAR(100)), '||' ,LOWER(TRIM(a.qnxt_specialty))) 
+end as qnxt_specialty
 from
 (
 SELECT 
@@ -20,5 +22,30 @@ SELECT
         CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
         CAST('' AS VARCHAR(100)) AS npiu2_concat
     FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
-    ) a
-   order by 1
+    ) a) tst1
+   left join
+   (
+   select b.npi, 
+case when b.SpecialityName = '' then b.sdnu1_concat
+else concat(cast(b.npi as VARCHAR(100)), '||' ,LOWER(TRIM(b.SpecialityName))) 
+end as SpecialityName
+from(
+SELECT 
+        CAST(sdnu1.NPI AS VARCHAR(100)) AS NPI,
+        CAST('' AS VARCHAR(100)) AS SpecialityName,
+        CONCAT(CAST(sdnu1.NPI AS VARCHAR(100)), '||A') AS sdnu1_concat
+    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu1
+    WHERE sdnu1.domainname = 'sPayer'
+    
+    UNION 
+    
+    SELECT 
+        CAST(sdnu2.NPI AS VARCHAR(100)) AS NPI,
+        CAST(sdnu2.SpecialityName AS VARCHAR(100)) AS SpecialityName,
+        CAST('' AS VARCHAR(100)) AS sdnu_concat
+    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu2
+    WHERE sdnu2.domainname = 'sPayer') b
+   ) tst2
+   on tst1.qnxt_specialty = tst2.SpecialityName
+)) npiu
+order by 1
