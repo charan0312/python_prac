4a53cde90041b802aa3990282244ad869cfde3bc
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 22:52:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-below is my query I'm getting nulls in specialty columns foix it for teradata
+SELECT * 
+FROM (
+    SELECT 
+        CAST(npiu2.NPI AS VARCHAR(100)) AS NPI,
+        CAST('' AS VARCHAR(100)) AS qnxt_specialty,
+        CONCAT(CAST(npiu2.NPI AS VARCHAR(100)), '||A') AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
 
-select * from
-(SELECT npiu2.NPI,'' as qnxt_specialty,
-concat(cast(npiu2.NPI as VARCHAR(100)), '||A' ) as npiu2_concat
-FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
-union all
-select npiu1.NPI, npiu1.qnxt_specialty , '' as npiu1_concat from HSLABCORNERSTONE.Directory_NPI_Universe npiu1) a
-order by 1
+    UNION ALL
+
+    SELECT 
+        CAST(npiu1.NPI AS VARCHAR(100)) AS NPI,
+        CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
+        CAST('' AS VARCHAR(100)) AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
+) a
+ORDER BY 1;
