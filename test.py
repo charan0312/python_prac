ae678029d75b2fe5fd7ceb541c8cce38cea4574e
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 22:55:05 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-select * from HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu1 where sdnu1.DomainName = 'sPayer'
-union
-SELECT distinct sdnu2.NPI,
-concat(cast(sdnu2.NPI as VARCHAR(100)), '||A' ) as SpecialityName
-FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu2
-where sdnu2.domainname = 'sPayer'
+SELECT * 
+FROM (
+    SELECT 
+        CAST(npiu2.NPI AS VARCHAR(100)) AS NPI,
+        CAST('' AS VARCHAR(100)) AS qnxt_specialty,
+        CONCAT(CAST(npiu2.NPI AS VARCHAR(100)), '||A') AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
+
+    UNION ALL
+
+    SELECT 
+        CAST(npiu1.NPI AS VARCHAR(100)) AS NPI,
+        CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
+        CAST('' AS VARCHAR(100)) AS npiu2_concat
+    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
+) a
+ORDER BY 1;
