4f72f183ed0cdb64e29b6bfbd81281ce2dd4dc65
Charan Parvathareddy <charan03.12@gmail.com>
2025-04-17 23:35:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,49 @@
-fix this
+SELECT TOP 5 * 
+FROM (
+    SELECT a.npi, 
+        CASE 
+            WHEN TRIM(a.qnxt_specialty) = '' THEN a.npiu2_concat
+            ELSE CONCAT(CAST(a.npi AS VARCHAR(100)), '||', LOWER(TRIM(CAST(a.qnxt_specialty AS VARCHAR(100)))))
+        END AS qnxt_specialty
+    FROM (
+        SELECT 
+            CAST(npiu2.NPI AS VARCHAR(100)) AS NPI,
+            CAST('' AS VARCHAR(100)) AS qnxt_specialty,
+            CONCAT(CAST(npiu2.NPI AS VARCHAR(100)), '||A') AS npiu2_concat
+        FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
 
+        UNION
 
-select top 5 * from 
-(
-select a.npi, 
-case when a.qnxt_specialty = '' then a.npiu2_concat
-else concat(cast(a.npi as VARCHAR(100)), '||' ,LOWER(TRIM(a.qnxt_specialty))) 
-end as qnxt_specialty
-from
-(
-SELECT 
-        CAST(npiu2.NPI AS VARCHAR(100)) AS NPI,
-        CAST('' AS VARCHAR(100)) AS qnxt_specialty,
-        CONCAT(CAST(npiu2.NPI AS VARCHAR(100)), '||A') AS npiu2_concat
-    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu2
+        SELECT 
+            CAST(npiu1.NPI AS VARCHAR(100)) AS NPI,
+            CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
+            CAST('' AS VARCHAR(100)) AS npiu2_concat
+        FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
+    ) a
+) tst1
+LEFT JOIN (
+    SELECT b.npi, 
+        CASE 
+            WHEN TRIM(b.SpecialityName) = '' THEN b.sdnu1_concat
+            ELSE CONCAT(CAST(b.npi AS VARCHAR(100)), '||', LOWER(TRIM(CAST(b.SpecialityName AS VARCHAR(100)))))
+        END AS SpecialityName
+    FROM (
+        SELECT 
+            CAST(sdnu1.NPI AS VARCHAR(100)) AS NPI,
+            CAST('' AS VARCHAR(100)) AS SpecialityName,
+            CONCAT(CAST(sdnu1.NPI AS VARCHAR(100)), '||A') AS sdnu1_concat
+        FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu1
+        WHERE sdnu1.domainname = 'sPayer'
 
-    UNION
+        UNION
 
-    SELECT 
-        CAST(npiu1.NPI AS VARCHAR(100)) AS NPI,
-        CAST(npiu1.qnxt_specialty AS VARCHAR(100)) AS qnxt_specialty,
-        CAST('' AS VARCHAR(100)) AS npiu2_concat
-    FROM HSLABCORNERSTONE.Directory_NPI_Universe npiu1
-    ) a) tst1
-   left join
-   (
-   select b.npi, 
-case when b.SpecialityName = '' then b.sdnu1_concat
-else concat(cast(b.npi as VARCHAR(100)), '||' ,LOWER(TRIM(b.SpecialityName))) 
-end as SpecialityName
-from(
-SELECT 
-        CAST(sdnu1.NPI AS VARCHAR(100)) AS NPI,
-        CAST('' AS VARCHAR(100)) AS SpecialityName,
-        CONCAT(CAST(sdnu1.NPI AS VARCHAR(100)), '||A') AS sdnu1_concat
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu1
-    WHERE sdnu1.domainname = 'sPayer'
-    
-    UNION 
-    
-    SELECT 
-        CAST(sdnu2.NPI AS VARCHAR(100)) AS NPI,
-        CAST(sdnu2.SpecialityName AS VARCHAR(100)) AS SpecialityName,
-        CAST('' AS VARCHAR(100)) AS sdnu_concat
-    FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu2
-    WHERE sdnu2.domainname = 'sPayer') b
-   ) tst2
-   on tst1.qnxt_specialty = tst2.SpecialityName
-)) npiu
-order by 1
+        SELECT 
+            CAST(sdnu2.NPI AS VARCHAR(100)) AS NPI,
+            CAST(sdnu2.SpecialityName AS VARCHAR(100)) AS SpecialityName,
+            CAST('' AS VARCHAR(100)) AS sdnu1_concat
+        FROM HSLABCORNERSTONE.SDIR_NPI_UNIVERSE sdnu2
+        WHERE sdnu2.domainname = 'sPayer'
+    ) b
+) tst2
+ON tst1.qnxt_specialty = tst2.SpecialityName
+ORDER BY 1;
